e9874bf17ded71f24441d5c15a0284d78c9fae7c
Thiago Jose Schiera <schierathiago@gmail.com>
2024-05-27 14:04:16 -0300

    Passwoer Checker Done

---
+++ Assets/LoginManager.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LoginManager : MonoBehaviour
+{
+    public string contra_seleccionada;
+    public InputField contra;
+    
+    
+
+
+    public void IngresoContraseña ()
+    {
+        if (contra.text == contra_seleccionada)
+        {
+            Debug.Log("Access granted");
+        }
+        else Debug.Log("Access denied");
+    }
+    
+}
